d5ea1144cb45c2ea55854767af9fdc1bb6678b47
blubber-rubber <ruben.vijverman@gmail.com>
2024-12-23 19:33:32 +0100

    Added solutions for day 22

--- Day22/part1.py
+++ Day22/part1.py
@@ -1,9 +1,32 @@
 import time
+import re
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+nodes = []
+for line in lines:
+    m = re.match('/dev/grid/node-x([0-9]+)-y([0-9]+)[^0-9]*([0-9]+)T[^0-9]*([0-9]+)T[^0-9]*([0-9]+)*T', line)
+    if m is not None:
+        x, y, s, u, a = (int(x) for x in m.groups())
+
+        nodes.append((x, y, u, a))
+
+nodes.sort(key=lambda x: -x[-1])
+
+index = 0
+available_pairs = 0
+
+
+for node in sorted(nodes, key=lambda x: -x[2]):
+    if node[2] != 0:
+        while index < len(nodes) and nodes[index][-1] > node[2]:
+            index += 1
+        available_pairs += index
+        if node[3] > node[2]:
+            available_pairs -= 1
+
+print(available_pairs)
 print(time.time() - start_time)
--- Day22/part2.py
+++ Day22/part2.py
@@ -1,9 +1,55 @@
 import time
+import re
+from heapq import heappush, heappop
+from collections import defaultdict
+import math
 
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+nodes = []
+for line in lines:
+    m = re.match('/dev/grid/node-x([0-9]+)-y([0-9]+)[^0-9]*([0-9]+)T[^0-9]*([0-9]+)T[^0-9]*([0-9]+)*T', line)
+    if m is not None:
+        x, y, s, u, a = (int(x) for x in m.groups())
+
+        nodes.append((x, y, u, a))
+
+N_COLS = max(n[0] for n in nodes) + 1
+N_ROWS = max(n[1] for n in nodes) + 1
+DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+def heuristic(pos):
+    return sum(pos)
+
+
+smalles_space = min(sum(n[-2:]) for n in nodes)
+
+walls = {(x, y) for x, y, u, a in nodes if u > smalles_space}
+
+open_pos = [(x, y) for x, y, u, a in nodes if u == 0][0]
+states = []
+d_pos = (N_COLS - 1, 0)
+heappush(states, (heuristic(d_pos), 0, d_pos, open_pos))
+visited = defaultdict(lambda: math.inf)
+
+while states[0][2] != (0, 0):
+    h, steps, d_pos, open_pos = heappop(states)
+    cox, coy = open_pos
+
+    if steps <= visited[(d_pos, open_pos)]:
+        for dx, dy in DIRS:
+            nx, ny = cox + dx, coy + dy
+            no_pos = (nx, ny)
+            nd_pos = d_pos
+            if no_pos == d_pos:
+                nd_pos = open_pos
+            if 0 <= nx < N_COLS and 0 <= ny < N_ROWS and no_pos not in walls and steps + 1 < visited[(nd_pos, no_pos)]:
+                visited[(nd_pos, no_pos)] = steps + 1
+                heappush(states, (steps + 1 + heuristic(nd_pos), steps + 1, nd_pos, no_pos))
+
+print(states[0][1])
 print(time.time() - start_time)
