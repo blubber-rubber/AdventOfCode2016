5d65e506f70af8d069bef860b4c8ebf522b7904e
blubber-rubber <ruben.vijverman@gmail.com>
2024-12-23 23:22:32 +0100

    Added solutions for day 25

--- Day25/part1.py
+++ Day25/part1.py
@@ -1,9 +1,38 @@
 import time
 
+
 start_time = time.time()
 
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+
+"""
+Today's solution was found by manually analyzing the input.txt file
+
+the input file did the following function
+
+"""
+
+
+def loop_once(a):
+    rem = 2550 + a
+    outputs = []
+    while rem != 0:
+        outputs.append(rem % 2)
+        rem = rem // 2
+
+    return outputs
+
+
+def check_loop(loop):
+    return len(loop) % 2 == 0 and all(sum(p) == 1 for p in zip(loop, loop[1:]))
+
+
+a = 0
+
+while not check_loop(loop_once(a)):
+    a += 1
+
+print(a)
 print(time.time() - start_time)
--- Day25/part2.py
+++ Day25/part2.py
@@ -5,5 +5,19 @@ start_time = time.time()
 with open('input.txt') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
-print(len(lines))
+"""
+Today's solution was found by analyzing the input.txt file
+
+
+Using the knowledge from part 1 we can speed our solution up further
+"""
+
+b = 0
+d = 1
+
+while b < 2550:
+    b, d = 2 * b + d, 1 - d
+
+print(b - 2550)
+
 print(time.time() - start_time)
